77bec0bf2ca2a1c3174b7bda5da941c3e434f87c
groupx2 <groupx2021@gmail.com>
2021-04-11 00:41:21 +0530

    dsdfsa

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -52,7 +52,7 @@ exports.getAvailabeRooms = catchAsync(async (req,res,next) => {
     httpOnly: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
     sameSite: "none",
-    domain: ''
+    domain: 'https://imperial-hotel.netlify.app'
   });
      req.query = Object.assign(req.query,{$or: [{"checkOut" : { lt : Date.now() }},{"checkOut" : { $eq : null}}]});
      next();
