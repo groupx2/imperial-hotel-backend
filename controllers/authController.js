4f02c3042a38b5010bfae28e9f49d3f6747149d1
groupx2 <groupx2021@gmail.com>
2021-04-11 02:14:11 +0530

    dsfsd

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,20 +16,20 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
-  const cookie = `jwt=${token}; samesite=none; secure`;
+ // const cookie = `jwt=${token}; samesite=none; secure`;
 
 
-  res.setHeader("set-cookie", [cookie]);
+ // res.setHeader("set-cookie", [cookie]);
   
 
-  // res.cookie('jwt', token, {
-  //   expires: new Date(
-  //     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-  //   ),
-  //   httpOnly: true,
-  //   secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-  //   sameSite: "none"
-  // });
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    sameSite: "none"
+  });
 
 
   // Remove password from output
