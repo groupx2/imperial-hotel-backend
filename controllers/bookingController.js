74dbe4828672b39891fc2d031e141b63180386a9
groupx2 <groupx2021@gmail.com>
2021-04-11 19:31:30 +0530

    fdsdf

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,12 +57,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   await Booking.create({ room, user, price });
 // });
 
-const createBookingCheckout = async session => {
+exports.createBookingCheckout = async session => {
   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
   const price = session.display_items[0].amount / 100;
   const room = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const {checkIn,checkOut} = session.metadata;
+
   await Booking.create({ room, user, price,checkIn,checkOut });
   await Room.findByIdAndUpdate(room,{checkIn,checkOut});
  
